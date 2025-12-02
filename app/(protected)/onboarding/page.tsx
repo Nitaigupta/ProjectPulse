9c1638b93795750e697630395c6cf80037aa8db0
Akash Yadav <114010679+akash-lifegod@users.noreply.github.com>
2025-12-02 16:21:31 +0530

    onboarding-form-done-and-corrected-subscription-schema

--- app/(protected)/onboarding/page.tsx
+++ app/(protected)/onboarding/page.tsx
@@ -1,11 +1,28 @@
+import { userRequired } from '@/app/data/user/is-user-authenticated';
 import { getUserWorkspaces } from '@/app/data/workspace/get-user-workspace';
+import { OnboardingForm } from '@/components/OnboardingForm';
 import { redirect } from 'next/navigation';
 import React from 'react'
 
 const page = async () => {
-  
+  const {data} = await getUserWorkspaces();
+  const {user} = await userRequired();
+
+  if(data?.onboardingCompleted && data?.workspaces.length > 0){
+    redirect("/workspace");
+  } else if(data?.onboardingCompleted){
+    redirect("/create-workspace");
+  }
+
+  const name = `${user?.given_name || ""} ${user?.family_name || ""}`;
   return (
-    <div>page</div>
+    <div className=''>
+      <OnboardingForm
+      name={name}
+      email={user?.email as string}
+      image={user?.picture || ""}
+      />
+    </div>
   )
 }
 
