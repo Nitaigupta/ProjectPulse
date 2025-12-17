7ff9bd23958a739d87e5bb12e1a9c17eb0850ccb
shuklapratham9 <130244305+shuklapratham9@users.noreply.github.com>
2025-12-17 19:00:46 +0530

    Added task-distribution-chart,progress-tracker,Light-to-Dark theme toggle. Completed Project Dashboard

--- components/projects/project-dashboard.tsx
+++ components/projects/project-dashboard.tsx
@@ -2,6 +2,10 @@ import { CommentProps, ProjectProps } from "@/utils/types"
 import {Activity, Task} from "@/lib/generated/prisma"
 import { ProjectHeader } from "./project-header"
 import { Card } from "../ui/card";
+import { TaskDistributionChart } from "./task-distribution-chart";
+import {ActivityFeed} from "./activity-feed"
+import { CommentList } from "./comment-list";
+import { CircleProgress } from "./circle-progress";
 interface ProjectDashboardProps {
     project:ProjectProps;
     tasks:{
@@ -18,24 +22,66 @@ interface ProjectDashboardProps {
 }
 
 export const ProjectDashboard = ({project,tasks,activities,totalWorkspaceMembers,comments}:ProjectDashboardProps) => {
+    console.log(tasks);
+    
     return <>
-    <div className="flex flec-col gap-6 px-2 md:px-4 2xl:px-6 py-0">
+    <div className="flex flex-col gap-6 px-2 md:px-4 2xl:px-6 py-0">
         <ProjectHeader project={project as unknown as ProjectProps}/>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cold-3 gap-3">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-8">
+       
+       <Card className="p-4">
+        <CircleProgress title="Task Completed" value={(tasks.completed/tasks.total)*100}
+       subTitle={`${tasks.completed}/${tasks.total} tasks `}
+       variant="success"
+       />
+       </Card>
 
-            {/* <Card></Card>
-            <div className="grid grid-cols-1 md:grid-cold-2 lg:grid-cold-3 gap-4">
-                <TaskDistributionChart/>
-                <Card>
-                    <h3 className="text-lg font-semibold mb-4">Recent Activities</h3>
-                    <ActivityFeed/>
-                </Card>
-                <Card>
-                    <h3 className="text-lg font-semibold mb-4">Recent Comments</h3>
-                    <CommentsList/>
-                </Card>
-            </div> */}
+       <Card className="p-4">
+        <CircleProgress title="In Progress" value={(tasks.inProgress/tasks.total)*100}
+       subTitle={`${tasks.inProgress} tasks ongoing`}
+       variant="inProgress"
+       />
+       </Card>
+
+       <Card className="p-4">
+        <CircleProgress title="Overdue" value={(tasks.overdue/tasks.total)*100}
+       subTitle={`${tasks.overdue} overdue tasks`}
+       variant="warning"
+       />
+       </Card>
+
+       <Card className="p-4">
+<CircleProgress title="Team Memebers" value={project.members.length}
+       subTitle={`${project.members.length} members`}
+       variant="default"
+       />
+       </Card>
+
+       
+
+       
+
+        </div>
+{/* grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4  */}
+        <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+
+         <TaskDistributionChart tasks={tasks}
+         />
+         <Card className="p-4">
+            <h3 className="text-lg font-semibold mb-4">Recent Activities</h3>
+            <ActivityFeed activities={activities.slice(0,5) as any}/>
+
+            
+         </Card>
+
+         <Card className="p-4">
+            <h3 className="text-lg font-semibold mb-4">Recent Comments</h3>
+            <CommentList comments={comments.slice(0,5) as any}/>
+
+            
+         </Card>
+
         </div>
 
     </div>
